143050eab71607d973c9e375ee6f83b173e8f579
uykusuz <vimevim@gmail.com>
2022-12-14 15:05:12 +0300

    Fixed import errors.

--- byproxy/__init__.py
+++ byproxy/__init__.py
@@ -4,9 +4,11 @@
 byproxy is a simple package containing simple tools for proxy management and usage.
 
 """
-from proxy_maker import ProxyMaker
-from proxy_checker import ProxyChecker
+from byproxy.proxy_maker import ProxyMaker
+from byproxy.proxy_checker import ProxyChecker
 
+__version__ = '0.1.0'
+__author__ = 'vimevim <vimevim@gmail.com>'
 __all__ = [
     'ProxyMaker',
     'ProxyChecker',
